d75d660e02e2718f372eae8626222714a4f4e15e
Mihailov <michhor8@gmail.com>
2023-06-10 15:04:07 +0200

    Header component WIP

--- storage-hub/src/app/components/Header.tsx
+++ storage-hub/src/app/components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from "./Header.module.css";
+import styles from "../../styles/components/Header.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,13 +8,36 @@ const Header = () => {
 
   return (
     <div className={styles.header}>
-      <div className={styles.logoContainer}></div>
+      <div className={styles.logoContainer}>
+        <div className={styles.logo}>
+          <Link href={"/"}>
+            {" "}
+            <Image src={"/logo.jpg"} alt="logo" width={130} height={90} />{" "}
+          </Link>
+        </div>
+      </div>
       <div className={styles.searchContainer}></div>
       <div className={styles.loginContainer}></div>
       <div
         className={styles.dropdownMenuContainer}
         onClick={() => setIsOpen(!isOpen)}
-      ></div>
+      >
+        <button>
+          <Image
+            src={"/icons/menuButton.png"}
+            width={30}
+            height={20}
+            alt="menu"
+          ></Image>
+        </button>
+        {isOpen && (
+          <ul className={styles.dropdownMenu}>
+            <li>Profile</li>
+            <li>My account</li>
+            <li>Logout</li>
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
--- storage-hub/src/app/page.tsx
+++ storage-hub/src/app/page.tsx
@@ -1,9 +1,13 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 import styles from "./page.module.css";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Header />
       <h1>Hello there</h1>
     </main>
   );
